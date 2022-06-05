79c81fb80ea5aeac63cd9251a9a22cd452118dd0
icYFTL <savap0@yandex.ru>
2022-06-05 20:38:29 +0300

    Fix

--- StandardShared/Logic/Models/GenericLogicResult.cs
+++ StandardShared/Logic/Models/GenericLogicResult.cs
@@ -5,6 +5,6 @@ namespace StandardShared.Logic.Models;
 public class GenericLogicResult
 {
     public virtual bool Status { get; init; }
-    public virtual string Message { get; init; } = String.Empty;
+    public virtual object? Result { get; init; } = String.Empty;
     public virtual HttpStatusCode HttpCode { get; init; } = HttpStatusCode.OK;
 }
